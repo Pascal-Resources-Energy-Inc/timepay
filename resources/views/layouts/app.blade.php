370c0b026a549f7b39c31e425e73c0a9ddfbf0d6
renzcabato1 <cabato.renz.renz@gmail.com>
2025-04-16 17:12:22 +0800

    Done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    @laravelPWA
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="shortcut icon" href="{{url('images/mini-icon.png')}}">
+    <link rel="icon" href="{{url('images/mini-icon.png')}}">
     <!-- Scripts -->
     {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
     {{-- <link rel="stylesheet" href="{{asset('login_css/fonts/icomoon/style.css')}}">

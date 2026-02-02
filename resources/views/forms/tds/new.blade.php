a65f61e76eb5a3a28a5462384287817cee17c10f
unknown <riemazoey14@gmail.com>
2026-02-02 11:01:06 +0800

    Remove required of fields

--- resources/views/forms/tds/new.blade.php
+++ resources/views/forms/tds/new.blade.php
@@ -151,10 +151,10 @@
           <div class="row">
             <div class="col-md-6">
               <div class="form-group">
-                <label>Postal Code <span class="text-danger">*</span></label>
+                <label>Postal Code</span></label>
                 <input type="text" class="form-control" 
                        name="postal_code" id="postal_code" value="{{ old('postal_code') }}" 
-                       placeholder="e.g., 1121" required>
+                       placeholder="e.g., 1121">
               </div>
             </div>
             <div class="col-md-6">
@@ -170,7 +170,7 @@
           <div class="row">
             <div class="col-md-12">
               <div class="form-group">
-                <label>Pin Exact Location <span class="text-danger">*</span></label>
+                <label>Pin Exact Location</span></label>
                 <div class="alert alert-warning d-flex align-items-start" role="alert">
                   <svg width="24" height="24" fill="currentColor" viewBox="0 0 16 16" style="min-width: 24px; margin-right: 10px;">
                     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>

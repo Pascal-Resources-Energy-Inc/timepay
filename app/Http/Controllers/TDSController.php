d8ea8db33684ce0d72885210e2c2710d1eb5920d
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-06-29 17:02:52 +0800

    fix: skip contact number duplicate check when customer_type is existing

--- app/Http/Controllers/TDSController.php
+++ app/Http/Controllers/TDSController.php
@@ -775,6 +775,9 @@ class TdsController extends Controller
         ]);
 
         $validator->after(function ($validator) use ($request) {
+            if ($request->customer_type !== 'new') {
+                return;
+            }
 
             $contactNo = preg_replace('/\s+/', '', trim((string) $request->contact_no));
 
@@ -790,10 +793,6 @@ class TdsController extends Controller
                         'This contact number already exists. Please select Existing Customer instead.'
                     );
                 }
-            }
-
-            if ($request->customer_type !== 'new') {
-                return;
             }
 
             $customerName = trim((string) $request->customer_name);
